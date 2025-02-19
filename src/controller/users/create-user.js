3d16d34499bb3ec3415c3721cf9db23c2c9c7b77
enhbayasgalan <enhuush0109@gmail.com>
2025-02-19 22:19:02 +0800

    fsdfs

--- src/controller/users/create-user.js
+++ src/controller/users/create-user.js
@@ -1,7 +1,7 @@
 import { users } from "./get-users.js"
 import fs from 'fs'
 export const  createUser =  (req , res) => {
-    const {firstName, seconName} = req.body
+    const {email, password} = req.body
     users.push({firstName, seconName})
     fs.writeFileSync("src/db/users.json", JSON.stringify(users))
     res.send(users)
