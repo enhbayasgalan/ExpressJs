d10cda7042d567d58d5eaf20320f9bf25f3af79b
enhbayasgalan <enhuush0109@gmail.com>
2025-02-20 21:35:36 +0800

    sf

--- src/routers/authentication.js
+++ src/routers/authentication.js
@@ -20,20 +20,20 @@ const valSignUpInput = (req, res, next) => {
   console.log(req);
 };
 
-// const valLoginInput = (req, res, next) => {
-//     console.log('middle ware running');
-//     const {email, password} = req.body;
-//     if (!email) {
-//         res.status(400).json({ error: "Bad request", message: "Email request"});
-//     } else if (!password) {
-//         res.status(400).json({ error: "Bad request", message: "Password request"})
-//     } else {
-//         next()
-//     }
-//     console.log(req);
-// }
+const valLoginInput = (req, res, next) => {
+    console.log('middle ware running');
+    const {email, password} = req.body;
+    if (!email) {
+        res.status(400).json({ error: "Bad request", message: "Email request"});
+    } else if (!password) {
+        res.status(400).json({ error: "Bad request", message: "Password request"})
+    } else {
+        next()
+    }
+    console.log(req);
+}
 
-// AuthenticationRouter.post('/login', valLoginInput, loginUser)
+AuthenticationRouter.post('/login', valLoginInput, loginUser)
 AuthenticationRouter.post("/sign-up", valSignUpInput, createUser);
 
 AuthenticationRouter.get("/", (req, res) => {
